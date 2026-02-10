8d15910aa7ca1ad5db168bddfd88e15d23723b37
Francesco <francesco.caruso1982@gmail.com>
2026-02-10 16:29:29 +0100

    Add PWA manifest and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,24 @@
 ﻿export const metadata = {
-  title: "Amemos",
-  description: "Gestione vendite Amemos",
+  title: "Amemos - Italia ODV",
+  description: "Amemos - inserimento vendite",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="it">
+      <head>
+        {/* PWA */}
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="theme-color" content="#111111" />
+
+        {/* iOS */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Amemos" />
+
+        {/* (Opzionale) icona iOS: usa quella 192 */}
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+      </head>
       <body
         style={{
           margin: 0,
@@ -23,3 +32,4 @@ export default function RootLayout({
     </html>
   );
 }
+
