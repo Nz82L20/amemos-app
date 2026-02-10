af5267970b7181c73fd1a9a2412b6bb0b39fdfb4
Francesco <francesco.caruso1982@gmail.com>
2026-02-10 15:20:41 +0100

    Remove old disabled app code

--- src/app_DISABLED/login/page.tsx
+++
@@ -1,8 +0,0 @@
-﻿export default function Login() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>LOGIN OK</h1>
-      <p>La route /login funziona ??</p>
-    </div>
-  );
-}
--- src/app_DISABLED/page.tsx
+++
@@ -1,3 +0,0 @@
-﻿export default function Home() {
-  return <div style={{ padding: 20 }}>HOME OK</div>;
-}
